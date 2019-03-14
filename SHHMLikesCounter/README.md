acb45dff90461abb086b2061810963b677882c55
Simon <simon@pixelpox.co.uk>
2019-03-14 09:07:03 +0000

    save data to text file

--- SHHMLikesCounter/README.md
+++ SHHMLikesCounter/README.md
@@ -5,6 +5,13 @@ SHHMakers
 https://en-gb.facebook.com/SHHMakers/
 ```
 
+Twitter ID is
+
+```
+shhmakers
+https://twitter.com/shhmakers?lang=en
+```
+
 # Sources
 
 1. https://www.hongkiat.com/blog/display-facebook-like-with-node-js/
--- SHHMLikesCounter/app.js
+++ SHHMLikesCounter/app.js
@@ -1,4 +1,5 @@
 var likes = require('likes')
+var fs = require("fs");
 
 likes.facebook('SHHMakers' , function (err , count)
 {
@@ -9,4 +10,26 @@ likes.facebook('SHHMakers' , function (err , count)
     }
 
     console.log('Sheffield Hardware Hackers and Makers has ' , count , ' likes on Facebook!')
+
+    var data = "New File Contents";
+
+    fs.writeFile("facebook.txt", count, function(err, data) 
+    {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
 })
+
+likes.twitter('shhmakers', function (err, count) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log('Sheffield Hardware Hackers and Makers has ', count, ' followers on Twitter')
+
+    fs.writeFile("twitter.txt", count, function(err, data) 
+    {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+  })
